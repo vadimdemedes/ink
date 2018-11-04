a0dd2b9eda7c90608ac6120ff9b85fb9e4039ac4
Vadim Demedes <vdemedes@gmail.com>
2018-11-04 15:14:57 -0800

    move render() to its own file

---
+++ src/render.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import undom from 'undom';
+import logUpdate from 'log-update';
+import debounce from 'debounce-fn';
+import createReconciler from './create-reconciler';
+import createRenderer from './create-renderer';
+import App from './components/App';
+
+export default (node, options = {}) => {
+	if (typeof options.write === 'function') {
+		options = {
+			stdout: options,
+			stdin: process.stdin
+		};
+	}
+
+	options = {
+		stdout: process.stdout,
+		stdin: process.stdin,
+		debug: false,
+		...options
+	};
+
+	const document = undom();
+	const render = createRenderer({
+		terminalWidth: options.stdout.columns
+	});
+
+	const log = logUpdate.create(options.stdout);
+
+	// Ignore last render after unmounting a tree to prevent empty output before exit
+	let ignoreRender = false;
+
+	const onRender = () => {
+		if (ignoreRender) {
+			return;
+		}
+
+		const output = render(document.body);
+
+		if (options.debug) {
+			options.stdout.write(output);
+			return;
+		}
+
+		log(output);
+	};
+
+	const debouncedRender = options.debug ? onRender : debounce(onRender);
+	const reconciler = createReconciler(document, debouncedRender);
+
+	if (!options.stdout._inkContainer) {
+		options.stdout._inkContainer = reconciler.createContainer(document.body, false);
+	}
+
+	const tree = (
+		<App stdin={options.stdin} stdout={options.stdout}>
+			{node}
+		</App>
+	);
+
+	reconciler.updateContainer(tree, options.stdout._inkContainer);
+
+	return () => {
+		if (typeof debouncedRender.cancel === 'function') {
+			debouncedRender.cancel();
+			onRender();
+			log.done();
+		}
+
+		ignoreRender = true;
+		reconciler.updateContainer(null, options.stdout._inkContainer);
+	};
+};
