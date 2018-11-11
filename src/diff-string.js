93977749e965806bc5a0a443e11dfac6cb27119a
Vadim Demedes <vdemedes@gmail.com>
2018-11-11 13:48:48 -0800

    Refactor diff-string helper for <Static> components

--- src/diff-string.js
+++ src/diff-string.js
@@ -1,3 +1,12 @@
+/**
+ * Naive string diff algorithm
+ *
+ * It's used to detect only new additions to the previous string and it expects previous string to never change.
+ * This function subtracts previous output from the new output and returns the difference.
+ *
+ * Used only for diffing output of <Static> component.
+ */
+
 export default (previous, next) => {
 	if (!previous) {
 		return next;
@@ -10,14 +19,14 @@ export default (previous, next) => {
 	const previousLines = previous.split('\n');
 	const nextLines = next.split('\n');
 
-	const lineCount = Math.max(previousLines.length, nextLines.length);
-	const diff = [];
-
-	for (let lineIndex = 0; lineIndex < lineCount; lineIndex++) {
-		if (previousLines[lineIndex] !== nextLines[lineIndex]) {
-			diff.push(nextLines[lineIndex]);
-		}
+	if (previousLines.length === nextLines.length) {
+		return next;
 	}
 
-	return diff.join('\n');
+	if (nextLines.length < previousLines.length) {
+		throw new TypeError('Output of <Static> component has become smaller. <Static> component requires existing children to stay the same in order to correctly detect new children and write them to output stream. Ensure that only new children get added to <Static> component and existing children produce the same output on every render.');
+	}
+
+	const diffStartIndex = nextLines.length - previousLines.length;
+	return nextLines.slice(diffStartIndex + 1).join('\n');
 };
