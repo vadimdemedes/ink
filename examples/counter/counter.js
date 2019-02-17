50a1178b8b7a3fbcacd39d47cd9b722043d4f20e
Vadim Demedes <vdemedes@gmail.com>
2019-02-16 18:02:31 -0800

    Add counter example

---
+++ examples/counter/counter.js
@@ -0,0 +1,35 @@
+'use strict';
+const React = require('react');
+const {render, Color} = require('../..');
+
+class Counter extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			i: 0
+		};
+	}
+
+	render() {
+		return (
+			<Color green>
+				{this.state.i} tests passed
+			</Color>
+		);
+	}
+
+	componentDidMount() {
+		this.timer = setInterval(() => {
+			this.setState({
+				i: this.state.i + 1
+			});
+		}, 100);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timer);
+	}
+}
+
+render(<Counter/>);
