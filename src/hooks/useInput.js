247da6248254723b5141816c3184ec6e7a7b83b1
Vadim Demedes <vdemedes@gmail.com>
2019-09-20 21:17:17 -0700

    Fixes

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -21,21 +21,21 @@ export default inputHandler => {
 				leftArrow: input === '\u001B[D',
 				rightArrow: input === '\u001B[C',
 				return: input === '\r',
-				escape: input === '\x1b',
+				escape: input === '\u001B',
 				ctrl: false,
 				shift: false
 			};
 
 			// Copied from `keypress` module
-			if (input <= '\x1a') {
-		    input = String.fromCharCode(input.charCodeAt(0) + 'a'.charCodeAt(0) - 1);
-		    key.ctrl = true;
-		  }
+			if (input <= '\u001A') {
+				input = String.fromCharCode(input.charCodeAt(0) + 'a'.charCodeAt(0) - 1);
+				key.ctrl = true;
+			}
 
-			const isLatinUppercase = input >= 'A' && input <= 'Z'
-			const isCyrillicUppercase = input => 'А' && input <= 'Я'
+			const isLatinUppercase = input >= 'A' && input <= 'Z';
+			const isCyrillicUppercase = input >= 'А' && input <= 'Я';
 			if (input.length === 1 && (isLatinUppercase || isCyrillicUppercase)) {
-			  key.shift = true;
+				key.shift = true;
 			}
 
 			inputHandler(input, key);
