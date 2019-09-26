8b073b4cee0e84ec32641af5f00300e2c85d05af
Vadim Demedes <vdemedes@gmail.com>
2019-09-25 20:43:20 -0700

    Update index.d.ts
    Co-Authored-By: Sindre Sorhus <sindresorhus@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -57,7 +57,7 @@ export type Unmount = () => void;
  * The callback you pass to `useInput` is called for each character when user enters any input.
  * However, if user pastes text and it's more than one character, the callback will be called only once and the whole string will be passed as `input`.
  *
- * ```js
+ * ```
  * import {useInput} from 'ink';
  *
  * const UserInput = () => {
