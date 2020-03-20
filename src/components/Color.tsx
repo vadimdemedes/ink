b6b5acdc8214aa1a50a6e9d6a7fb41224bde3cb4
Taras Mankovski <tarasm@gmail.com>
2020-03-20 19:06:56 -0400

    Remove ts-ignore from Color component

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -1,7 +1,8 @@
 import React, {FC, ReactNode} from 'react';
 import PropTypes from 'prop-types';
 import arrify from 'arrify';
-import chalk from 'chalk';
+import chalk, {Chalk} from 'chalk';
+import {Except} from 'type-fest';
 
 const methods = [
 	'hex',
@@ -15,31 +16,36 @@ const methods = [
 	'bgHsv',
 	'bgHwb',
 	'bgRgb',
-	'bgKeyword'
+	'bgKeyword',
+	'ansi',
+	'ansi256',
+	'bgAnsi',
+	'bgAnsi256'
 ];
 
-const Color: FC<ColorProps & { children: ReactNode }> = ({children, ...colorProps}) => {
+const Color: FC<ColorProps & { children: ReactNode }> = ({
+	children,
+	...colorProps
+}) => {
 	if (children === '') {
 		return null;
 	}
 
-	// TODO: Figure out what's going on here and write the type definition
-	const transformChildren = (children: ReactNode) => {
-		Object.keys(colorProps).forEach(method => {
-			// @ts-ignore
-			if (colorProps[method]) {
-				if (methods.includes(method)) {
-					// @ts-ignore
-					children = chalk[method](...arrify(colorProps[method]))(children);
-					// @ts-ignore
-				} else if (typeof chalk[method] === 'function') {
-					// @ts-ignore
-					children = chalk[method](children);
+	const transformChildren = (children: string) => {
+		return Object.keys(colorProps).reduce(
+			(acc: string, methodName: keyof ColorProps) => {
+				if (methods.includes(methodName)) {
+					return (chalk[methodName] as any)(...arrify(colorProps[methodName]))(acc);
 				}
-			}
-		});
 
-		return children;
+				if (typeof chalk[methodName] === 'function') {
+					return (chalk[methodName] as any)(children);
+				}
+
+				return acc;
+			},
+			children
+		);
 	};
 
 	return (
@@ -63,64 +69,17 @@ Color.defaultProps = {
 
 export default Color;
 
-interface ColorProps {
-	readonly hex?: string;
-	readonly hsl?: [number, number, number];
-	readonly hsv?: [number, number, number];
-	readonly hwb?: [number, number, number];
-	readonly rgb?: [number, number, number];
-	readonly keyword?: string;
-	readonly bgHex?: string;
-	readonly bgHsl?: [number, number, number];
-	readonly bgHsv?: [number, number, number];
-	readonly bgHwb?: [number, number, number];
-	readonly bgRgb?: [number, number, number];
-	readonly bgKeyword?: string;
+type Colors =
+	| typeof chalk.ForegroundColor
+	| typeof chalk.BackgroundColor
+	| typeof chalk.Modifiers;
 
-	readonly reset?: boolean;
-	readonly bold?: boolean;
-	readonly dim?: boolean;
-	readonly italic?: boolean;
-	readonly underline?: boolean;
-	readonly inverse?: boolean;
-	readonly hidden?: boolean;
-	readonly strikethrough?: boolean;
+type ChalkFunctions = Except<Except<Chalk, 'Instance' | 'level'>, Colors>;
 
-	readonly visible?: boolean;
+type ChalkFunctionProps = {
+	[K in keyof ChalkFunctions]: Parameters<ChalkFunctions[K]>;
+};
 
-	readonly black?: boolean;
-	readonly red?: boolean;
-	readonly green?: boolean;
-	readonly yellow?: boolean;
-	readonly blue?: boolean;
-	readonly magenta?: boolean;
-	readonly cyan?: boolean;
-	readonly white?: boolean;
-	readonly gray?: boolean;
-	readonly grey?: boolean;
-	readonly blackBright?: boolean;
-	readonly redBright?: boolean;
-	readonly greenBright?: boolean;
-	readonly yellowBright?: boolean;
-	readonly blueBright?: boolean;
-	readonly magentaBright?: boolean;
-	readonly cyanBright?: boolean;
-	readonly whiteBright?: boolean;
+type ChalkBooleanProps = Record<Colors, boolean>;
 
-	readonly bgBlack?: boolean;
-	readonly bgRed?: boolean;
-	readonly bgGreen?: boolean;
-	readonly bgYellow?: boolean;
-	readonly bgBlue?: boolean;
-	readonly bgMagenta?: boolean;
-	readonly bgCyan?: boolean;
-	readonly bgWhite?: boolean;
-	readonly bgBlackBright?: boolean;
-	readonly bgRedBright?: boolean;
-	readonly bgGreenBright?: boolean;
-	readonly bgYellowBright?: boolean;
-	readonly bgBlueBright?: boolean;
-	readonly bgMagentaBright?: boolean;
-	readonly bgCyanBright?: boolean;
-	readonly bgWhiteBright?: boolean;
-}
+type ColorProps = Partial<ChalkBooleanProps & ChalkFunctionProps>;
