ce79b7560a3ce9c6aff96778caae9c037b583af5
Taras Mankovski <tarasm@gmail.com>
2020-03-28 13:50:48 -0400

    Renamed AppContextValue to AppContextProps

--- src/components/AppContext.ts
+++ src/components/AppContext.ts
@@ -1,12 +1,12 @@
 import {createContext} from 'react';
 
-export interface AppContextValue {
+export interface AppContextProps {
 	exit: (error?: Error) => void;
 }
 
 /**
  * `AppContext` is a React context, which exposes a method to manually exit the app (unmount).
  */
-export const AppContext = createContext<AppContextValue>({
+export const AppContext = createContext<AppContextProps>({
 	exit: () => {}
 });
