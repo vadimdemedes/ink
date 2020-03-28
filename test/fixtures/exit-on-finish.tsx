0eabce011b23eca35b02fbc7c0c40d5ce78e8d96
Taras Mankovski <tarasm@gmail.com>
2020-03-28 13:02:23 -0400

    Update test/fixtures/exit-on-finish.tsx
    Co-Authored-By: Sindre Sorhus <sindresorhus@gmail.com>

--- test/fixtures/exit-on-finish.tsx
+++ test/fixtures/exit-on-finish.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render, Box} from '../../src';
 
-class Test extends React.Component<Record<string, unknown>, { counter: number }> {
+class Test extends React.Component<Record<string, unknown>, {counter: number}> {
 	timer?: NodeJS.Timeout;
 
 	state = {
