d96e6388bf03ad32c2a071a91bc8290f1bcb7c9f
779g <adrian.villa@779.mx>
2020-11-29 21:10:22 +0000

    Examples correctedto right linting style, added missing import

--- examples/router/google-analytics.js
+++ examples/router/google-analytics.js
@@ -1,67 +1,65 @@
 import * as React from 'react';
+import {useEffect} from 'react';
 import ua from 'universal-analytics';
 
 import {
-  MemoryRouter,
-  Switch,
-  Route,
-  useHistory,
-  useLocation
+	MemoryRouter,
+	Switch,
+	Route,
+	useHistory,
+	useLocation
 } from 'react-router';
 
-import { Text, Newline, Box, useInput } from 'ink';
+import {Text, Newline, Box, useInput} from 'ink';
 
-var visitor = ua('UA-00000000-0')
+const visitor = ua('UA-00000000-0');
 
 const App = () => {
+	const history = useHistory();
 
-  const history = useHistory();
+	const location = useLocation();
 
-  const location = useLocation();
+	useEffect(() => {
+		visitor.pageview(location.pathname + location.search).send();
+	}, [location.pathname, location.search]);
 
-  useEffect(() => {
-    visitor.pageview(location.pathname + location.search).send();
-  }, [location.pathname, location.search]);
+	useInput((input, key) => {
+		if (key.return) {
+			history.push('/main');
+		}
 
-  useInput((input, key) => {
-    if (key.return) {
-      history.push('/main');
-    };
-    if (input === 'h') {
-      history.push('/help');
-    };
+		if (input === 'h') {
+			history.push('/help');
+		}
+	});
 
-  });
-
-  return(
-    <>
-      <Switch>
-        <Route path='/main'>
-          <Text>The main program executes here</Text>
-        </Route>
-        <Route path='/help'>
-          <Text>This is the help section</Text>
-        </Route>
-      </Switch>
-      <Box margin={2}>
-        <Text>
-          <Text bold>Menu</Text>
-          <Newline/>
-          <Text>Press h for help</Text>
-          <Newline/>
-          <Text>Or press enter to continue</Text>
-        </Text>
-      </Box>
-    </>
-
-  );
+	return (
+		<>
+			<Switch>
+				<Route path="/main">
+					<Text>The main program executes here</Text>
+				</Route>
+				<Route path="/help">
+					<Text>This is the help section</Text>
+				</Route>
+			</Switch>
+			<Box margin={2}>
+				<Text>
+					<Text bold>Menu</Text>
+					<Newline />
+					<Text>Press h for help</Text>
+					<Newline />
+					<Text>Or press enter to continue</Text>
+				</Text>
+			</Box>
+		</>
+	);
 };
 
 export default () => {
-
-  return(
-    <MemoryRouter>
-        <App />
-    </MemoryRouter>
-  );
+	return (
+		<MemoryRouter>
+			<App />
+		</MemoryRouter>
+	);
 };
