ece996911c187b1ce3c601315c84cfb3e6219715
inokawa <48897392+inokawa@users.noreply.github.com>
2020-12-13 02:23:29 +0900

    Fix typo (#405)

--- src/hooks/use-input.ts
+++ src/hooks/use-input.ts
@@ -90,7 +90,7 @@ interface Options {
 
 /**
  * This hook is used for handling user input.
- * It's a more convienient alternative to using `StdinContext` and listening to `data` events.
+ * It's a more convenient alternative to using `StdinContext` and listening to `data` events.
  * The callback you pass to `useInput` is called for each character when user enters any input.
  * However, if user pastes text and it's more than one character, the callback will be called only once and the whole string will be passed as `input`.
  *
