aaae37349cebf12be5a2419d66ab594cfc8bec1b
Kostya <kostya@kostya.local>
2020-12-19 14:59:40 +0200

    Refactored code

--- examples/subprocess-output/subprocess-output.js
+++ examples/subprocess-output/subprocess-output.js
@@ -1,34 +1,29 @@
 'use strict';
 const React = require('react');
+const stripAnsi = require('strip-ansi');
 const {render, Text, Box} = require('../..');
 const childProcess = require('child_process');
 
 const SubprocessOutput = () => {
 	const [output, setOutput] = React.useState('');
-	const [outputArray, setOutputArray] = React.useState([]);
 
 	React.useEffect(() => {
-		const subProcess = childProcess.spawn('ping', ['8.8.8.8']);
+		const subProcess = childProcess.spawn('traceroute', ['google.com']);
 
 		subProcess.stdout.on('data', newOutput => {
-			setOutput(newOutput.toString('ascii'));
+			const lines = stripAnsi(newOutput.toString('utf8')).split('\n');
+			setOutput(lines.slice(0, 5).join('\n'));
 		});
 	}, [setOutput]);
-
-	React.useEffect(() => {
-		setOutputArray([...outputArray, output].slice(-5));
-	}, [setOutputArray, output]);
 
 	return (
 		<Box flexDirection="column" padding={1}>
 			<Text>
 				My <Text bold>ping</Text> output:
 			</Text>
-			{outputArray.map((output, i) => (
-				<Box key={i} marginTop="1">
-					<Text>{output}</Text>
-				</Box>
-			))}
+			<Box marginTop="1">
+				<Text>{output}</Text>
+			</Box>
 		</Box>
 	);
 };
