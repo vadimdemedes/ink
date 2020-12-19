7aee66be2494f162b91ba0f47cd7446298924ebc
Kostya <kostya@kostya.local>
2020-12-19 14:37:18 +0200

    Refactored code

--- examples/subprocess-output/subprocess-output.js
+++ examples/subprocess-output/subprocess-output.js
@@ -1,23 +1,34 @@
 'use strict';
 const React = require('react');
-const {render, Text, Box} = require('../../build');
+const {render, Text, Box} = require('../..');
 const childProcess = require('child_process');
 
 const SubprocessOutput = () => {
 	const [output, setOutput] = React.useState('');
+	const [outputArray, setOutputArray] = React.useState([]);
 
 	React.useEffect(() => {
-		const subProcess = childProcess.spawn('ping', ['8.8.8.8']).stdout;
+		const subProcess = childProcess.spawn('ping', ['8.8.8.8']);
 
-		subProcess.on('data', newOutput => {
+		subProcess.stdout.on('data', newOutput => {
 			setOutput(newOutput.toString('ascii'));
 		});
 	}, [setOutput]);
 
+	React.useEffect(() => {
+		setOutputArray([...outputArray, output].slice(-5));
+	}, [setOutputArray, output]);
+
 	return (
-		<Box flexDirection="column">
-			<Text>My ping output:</Text>
-			<Text>{output}</Text>
+		<Box flexDirection="column" padding={1}>
+			<Text>
+				My <Text bold>ping</Text> output:
+			</Text>
+			{outputArray.map((output, i) => (
+				<Box key={i} marginTop="1">
+					<Text>{output}</Text>
+				</Box>
+			))}
 		</Box>
 	);
 };
