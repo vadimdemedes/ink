e715583f9cba664862124d1d103ad2c63067b5c5
Prozacgod <prozacgod@gmail.com>
2021-01-24 23:08:21 -0600

    Added fullscreen component that tracks screen resizes and wraps children in a box

---
+++ src/components/Fullscreen.tsx
@@ -0,0 +1,33 @@
+import React, {FC, useState, useEffect} from 'react';
+import Box from './Box';
+
+const Fullscreen: FC = ({children}) => {
+	const [size, setSize] = useState({
+		columns: process.stdout.columns,
+		rows: process.stdout.rows
+	});
+
+	useEffect(() => {
+		function onResize() {
+			setSize({
+				columns: process.stdout.columns,
+				rows: process.stdout.rows
+			});
+		}
+
+		process.stdout.on('resize', onResize);
+		process.stdout.write('\x1b[?1049h');
+		return () => {
+			process.stdout.off('resize', onResize);
+			process.stdout.write('\x1b[?1049l');
+		};
+	}, []);
+
+	return (
+		<Box width={size.columns} height={size.rows}>
+			{children}
+		</Box>
+	);
+};
+
+export default Fullscreen;
