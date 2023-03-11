6278b816ddd4d52e20c53cdf6e42cf81942778ac
Vadim Demedes <vadimdemedes@hey.com>
2023-03-11 12:51:32 +0200

    Allow configuring overflow behavior in `Box` component (#502)

--- src/output.ts
+++ src/output.ts
@@ -1,5 +1,6 @@
 import sliceAnsi from 'slice-ansi';
 import stringWidth from 'string-width';
+import widestLine from 'widest-line';
 import {type OutputTransformer} from './render-node-to-output.js';
 
 /**
@@ -16,19 +17,37 @@ type Options = {
 	height: number;
 };
 
-type Writes = {
+type Operation = WriteOperation | ClipOperation | UnclipOperation;
+
+type WriteOperation = {
+	type: 'write';
 	x: number;
 	y: number;
 	text: string;
 	transformers: OutputTransformer[];
 };
 
+type ClipOperation = {
+	type: 'clip';
+	clip: Clip;
+};
+
+type Clip = {
+	x1: number | undefined;
+	x2: number | undefined;
+	y1: number | undefined;
+	y2: number | undefined;
+};
+
+type UnclipOperation = {
+	type: 'unclip';
+};
+
 export default class Output {
 	width: number;
 	height: number;
 
-	// Initialize output array with a specific set of rows, so that margin/padding at the bottom is preserved
-	private readonly writes: Writes[] = [];
+	private readonly operations: Operation[] = [];
 
 	constructor(options: Options) {
 		const {width, height} = options;
@@ -49,41 +68,129 @@ export default class Output {
 			return;
 		}
 
-		this.writes.push({x, y, text, transformers});
+		this.operations.push({
+			type: 'write',
+			x,
+			y,
+			text,
+			transformers
+		});
+	}
+
+	clip(clip: Clip) {
+		this.operations.push({
+			type: 'clip',
+			clip
+		});
+	}
+
+	unclip() {
+		this.operations.push({
+			type: 'unclip'
+		});
 	}
 
 	get(): {output: string; height: number} {
+		// Initialize output array with a specific set of rows, so that margin/padding at the bottom is preserved
 		const output: string[] = [];
 
 		for (let y = 0; y < this.height; y++) {
 			output.push(' '.repeat(this.width));
 		}
 
-		for (const write of this.writes) {
-			const {x, y, text, transformers} = write;
-			const lines = text.split('\n');
-			let offsetY = 0;
+		const clips: Clip[] = [];
 
-			for (let line of lines) {
-				const currentLine = output[y + offsetY];
+		for (const operation of this.operations) {
+			if (operation.type === 'clip') {
+				clips.push(operation.clip);
+			}
 
-				// Line can be missing if `text` is taller than height of pre-initialized `this.output`
-				if (!currentLine) {
-					continue;
+			if (operation.type === 'unclip') {
+				clips.pop();
+			}
+
+			if (operation.type === 'write') {
+				const {text, transformers} = operation;
+				let {x, y} = operation;
+				let lines = text.split('\n');
+
+				const clip = clips[clips.length - 1];
+
+				if (clip) {
+					const clipHorizontally =
+						typeof clip?.x1 === 'number' && typeof clip?.x2 === 'number';
+
+					const clipVertically =
+						typeof clip?.y1 === 'number' && typeof clip?.y2 === 'number';
+
+					// If text is positioned outside of clipping area altogether,
+					// skip to the next operation to avoid unnecessary calculations
+					if (clipHorizontally) {
+						const width = widestLine(text);
+
+						if (x + width < clip.x1! || x > clip.x2!) {
+							continue;
+						}
+					}
+
+					if (clipVertically) {
+						const height = lines.length;
+
+						if (y + height < clip.y1! || y > clip.y2!) {
+							continue;
+						}
+					}
+
+					if (clipHorizontally) {
+						lines = lines.map(line => {
+							const from = x < clip.x1! ? clip.x1! - x : 0;
+							const width = stringWidth(line);
+							const to = x + width > clip.x2! ? clip.x2! - x : width;
+
+							return sliceAnsi(line, from, to);
+						});
+
+						if (x < clip.x1!) {
+							x = clip.x1!;
+						}
+					}
+
+					if (clipVertically) {
+						const from = y < clip.y1! ? clip.y1! - y : 0;
+						const height = lines.length;
+						const to = y + height > clip.y2! ? clip.y2! - y : height;
+
+						lines = lines.slice(from, to);
+
+						if (y < clip.y1!) {
+							y = clip.y1!;
+						}
+					}
 				}
 
-				const width = stringWidth(line);
+				let offsetY = 0;
 
-				for (const transformer of transformers) {
-					line = transformer(line);
+				for (let line of lines) {
+					const currentLine = output[y + offsetY];
+
+					// Line can be missing if `text` is taller than height of pre-initialized `this.output`
+					if (!currentLine) {
+						continue;
+					}
+
+					const width = stringWidth(line);
+
+					for (const transformer of transformers) {
+						line = transformer(line);
+					}
+
+					output[y + offsetY] =
+						sliceAnsi(currentLine, 0, x) +
+						line +
+						sliceAnsi(currentLine, x + width);
+
+					offsetY++;
 				}
-
-				output[y + offsetY] =
-					sliceAnsi(currentLine, 0, x) +
-					line +
-					sliceAnsi(currentLine, x + width);
-
-				offsetY++;
 			}
 		}
 
---
+++ test/overflow.tsx
@@ -0,0 +1,503 @@
+import React from 'react';
+import test from 'ava';
+import boxen, {type Options} from 'boxen';
+import sliceAnsi from 'slice-ansi';
+import {Box, Text} from '../src/index.js';
+import {renderToString} from './helpers/render-to-string.js';
+
+const box = (text: string, options?: Options): string => {
+	return boxen(text, {
+		...options,
+		borderStyle: 'round'
+	});
+};
+
+const clipX = (text: string, columns: number): string => {
+	return text
+		.split('\n')
+		.map(line => sliceAnsi(line, 0, columns).trim())
+		.join('\n');
+};
+
+test('overflowX - single text node in a box inside overflow container', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden">
+			<Box width={16} flexShrink={0}>
+				<Text>Hello World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'Hello');
+});
+
+test('overflowX - single text node inside overflow container with border', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden" borderStyle="round">
+			<Box width={16} flexShrink={0}>
+				<Text>Hello World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box('Hell'));
+});
+
+test('overflowX - single text node in a box with border inside overflow container', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden">
+			<Box width={16} flexShrink={0} borderStyle="round">
+				<Text>Hello World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, clipX(box('Hello'), 6));
+});
+
+test('overflowX - multiple text nodes in a box inside overflow container', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden">
+			<Box width={12} flexShrink={0}>
+				<Text>Hello </Text>
+				<Text>World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'Hello');
+});
+
+test('overflowX - multiple text nodes in a box inside overflow container with border', t => {
+	const output = renderToString(
+		<Box width={8} overflowX="hidden" borderStyle="round">
+			<Box width={12} flexShrink={0}>
+				<Text>Hello </Text>
+				<Text>World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box('Hello '));
+});
+
+test('overflowX - multiple text nodes in a box with border inside overflow container', t => {
+	const output = renderToString(
+		<Box width={8} overflowX="hidden">
+			<Box width={12} flexShrink={0} borderStyle="round">
+				<Text>Hello </Text>
+				<Text>World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, clipX(box('HelloWo\n'), 8));
+});
+
+test('overflowX - multiple boxes inside overflow container', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden">
+			<Box width={6} flexShrink={0}>
+				<Text>Hello </Text>
+			</Box>
+			<Box width={6} flexShrink={0}>
+				<Text>World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'Hello');
+});
+
+test('overflowX - multiple boxes inside overflow container with border', t => {
+	const output = renderToString(
+		<Box width={8} overflowX="hidden" borderStyle="round">
+			<Box width={6} flexShrink={0}>
+				<Text>Hello </Text>
+			</Box>
+			<Box width={6} flexShrink={0}>
+				<Text>World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box('Hello '));
+});
+
+test('overflowX - box before left edge of overflow container', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden">
+			<Box marginLeft={-12} width={6} flexShrink={0}>
+				<Text>Hello</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, '');
+});
+
+test('overflowX - box before left edge of overflow container with border', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden" borderStyle="round">
+			<Box marginLeft={-12} width={6} flexShrink={0}>
+				<Text>Hello</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box(' '.repeat(4)));
+});
+
+test('overflowX - box intersecting with left edge of overflow container', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden">
+			<Box marginLeft={-3} width={12} flexShrink={0}>
+				<Text>Hello World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'lo Wor');
+});
+
+test('overflowX - box intersecting with left edge of overflow container with border', t => {
+	const output = renderToString(
+		<Box width={8} overflowX="hidden" borderStyle="round">
+			<Box marginLeft={-3} width={12} flexShrink={0}>
+				<Text>Hello World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box('lo Wor'));
+});
+
+test('overflowX - box after right edge of overflow container', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden">
+			<Box marginLeft={6} width={6} flexShrink={0}>
+				<Text>Hello</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, '');
+});
+
+test('overflowX - box intersecting with right edge of overflow container', t => {
+	const output = renderToString(
+		<Box width={6} overflowX="hidden">
+			<Box marginLeft={3} width={6} flexShrink={0}>
+				<Text>Hello</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, '   Hel');
+});
+
+test('overflowY - single text node inside overflow container', t => {
+	const output = renderToString(
+		<Box height={1} overflowY="hidden">
+			<Text>Hello{'\n'}World</Text>
+		</Box>
+	);
+
+	t.is(output, 'Hello');
+});
+
+test('overflowY - single text node inside overflow container with border', t => {
+	const output = renderToString(
+		<Box width={20} height={3} overflowY="hidden" borderStyle="round">
+			<Text>Hello{'\n'}World</Text>
+		</Box>
+	);
+
+	t.is(output, box('Hello'.padEnd(18, ' ')));
+});
+
+test('overflowY - multiple boxes inside overflow container', t => {
+	const output = renderToString(
+		<Box height={2} overflowY="hidden" flexDirection="column">
+			<Box flexShrink={0}>
+				<Text>Line #1</Text>
+			</Box>
+			<Box flexShrink={0}>
+				<Text>Line #2</Text>
+			</Box>
+			<Box flexShrink={0}>
+				<Text>Line #3</Text>
+			</Box>
+			<Box flexShrink={0}>
+				<Text>Line #4</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'Line #1\nLine #2');
+});
+
+test('overflowY - multiple boxes inside overflow container with border', t => {
+	const output = renderToString(
+		<Box
+			width={9}
+			height={4}
+			overflowY="hidden"
+			flexDirection="column"
+			borderStyle="round"
+		>
+			<Box flexShrink={0}>
+				<Text>Line #1</Text>
+			</Box>
+			<Box flexShrink={0}>
+				<Text>Line #2</Text>
+			</Box>
+			<Box flexShrink={0}>
+				<Text>Line #3</Text>
+			</Box>
+			<Box flexShrink={0}>
+				<Text>Line #4</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box('Line #1\nLine #2'));
+});
+
+test('overflowY - box above top edge of overflow container', t => {
+	const output = renderToString(
+		<Box height={1} overflowY="hidden">
+			<Box marginTop={-2} height={2} flexShrink={0}>
+				<Text>Hello{'\n'}World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, '');
+});
+
+test('overflowY - box above top edge of overflow container with border', t => {
+	const output = renderToString(
+		<Box width={7} height={3} overflowY="hidden" borderStyle="round">
+			<Box marginTop={-3} height={2} flexShrink={0}>
+				<Text>Hello{'\n'}World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box(' '.repeat(5)));
+});
+
+test('overflowY - box intersecting with top edge of overflow container', t => {
+	const output = renderToString(
+		<Box height={1} overflowY="hidden">
+			<Box marginTop={-1} height={2} flexShrink={0}>
+				<Text>Hello{'\n'}World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'World');
+});
+
+test('overflowY - box intersecting with top edge of overflow container with border', t => {
+	const output = renderToString(
+		<Box width={7} height={3} overflowY="hidden" borderStyle="round">
+			<Box marginTop={-1} height={2} flexShrink={0}>
+				<Text>Hello{'\n'}World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box('World'));
+});
+
+test('overflowY - box below bottom edge of overflow container', t => {
+	const output = renderToString(
+		<Box height={1} overflowY="hidden">
+			<Box marginTop={1} height={2} flexShrink={0}>
+				<Text>Hello{'\n'}World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, '');
+});
+
+test('overflowY - box below bottom edge of overflow container with border', t => {
+	const output = renderToString(
+		<Box width={7} height={3} overflowY="hidden" borderStyle="round">
+			<Box marginTop={2} height={2} flexShrink={0}>
+				<Text>Hello{'\n'}World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box(' '.repeat(5)));
+});
+
+test('overflowY - box intersecting with bottom edge of overflow container', t => {
+	const output = renderToString(
+		<Box height={1} overflowY="hidden">
+			<Box height={2} flexShrink={0}>
+				<Text>Hello{'\n'}World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'Hello');
+});
+
+test('overflowY - box intersecting with bottom edge of overflow container with border', t => {
+	const output = renderToString(
+		<Box width={7} height={3} overflowY="hidden" borderStyle="round">
+			<Box height={2} flexShrink={0}>
+				<Text>Hello{'\n'}World</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, box('Hello'));
+});
+
+test('overflow - single text node inside overflow container', t => {
+	const output = renderToString(
+		<Box paddingBottom={1}>
+			<Box width={6} height={1} overflow="hidden">
+				<Box width={12} height={2} flexShrink={0}>
+					<Text>Hello{'\n'}World</Text>
+				</Box>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'Hello\n');
+});
+
+test('overflow - single text node inside overflow container with border', t => {
+	const output = renderToString(
+		<Box paddingBottom={1}>
+			<Box width={8} height={3} overflow="hidden" borderStyle="round">
+				<Box width={12} height={2} flexShrink={0}>
+					<Text>Hello{'\n'}World</Text>
+				</Box>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, `${box('Hello ')}\n`);
+});
+
+test('overflow - multiple boxes inside overflow container', t => {
+	const output = renderToString(
+		<Box paddingBottom={1}>
+			<Box width={4} height={1} overflow="hidden">
+				<Box width={2} height={2} flexShrink={0}>
+					<Text>TL{'\n'}BL</Text>
+				</Box>
+				<Box width={2} height={2} flexShrink={0}>
+					<Text>TR{'\n'}BR</Text>
+				</Box>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'TLTR\n');
+});
+
+test('overflow - multiple boxes inside overflow container with border', t => {
+	const output = renderToString(
+		<Box paddingBottom={1}>
+			<Box width={6} height={3} overflow="hidden" borderStyle="round">
+				<Box width={2} height={2} flexShrink={0}>
+					<Text>TL{'\n'}BL</Text>
+				</Box>
+				<Box width={2} height={2} flexShrink={0}>
+					<Text>TR{'\n'}BR</Text>
+				</Box>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, `${box('TLTR')}\n`);
+});
+
+test('overflow - box intersecting with top left edge of overflow container', t => {
+	const output = renderToString(
+		<Box width={4} height={4} overflow="hidden">
+			<Box marginTop={-2} marginLeft={-2} width={4} height={4} flexShrink={0}>
+				<Text>
+					AAAA{'\n'}BBBB{'\n'}CCCC{'\n'}DDDD
+				</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'CC\nDD\n\n');
+});
+
+test('overflow - box intersecting with top right edge of overflow container', t => {
+	const output = renderToString(
+		<Box width={4} height={4} overflow="hidden">
+			<Box marginTop={-2} marginLeft={2} width={4} height={4} flexShrink={0}>
+				<Text>
+					AAAA{'\n'}BBBB{'\n'}CCCC{'\n'}DDDD
+				</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, '  CC\n  DD\n\n');
+});
+
+test('overflow - box intersecting with bottom left edge of overflow container', t => {
+	const output = renderToString(
+		<Box width={4} height={4} overflow="hidden">
+			<Box marginTop={2} marginLeft={-2} width={4} height={4} flexShrink={0}>
+				<Text>
+					AAAA{'\n'}BBBB{'\n'}CCCC{'\n'}DDDD
+				</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, '\n\nAA\nBB');
+});
+
+test('overflow - box intersecting with bottom right edge of overflow container', t => {
+	const output = renderToString(
+		<Box width={4} height={4} overflow="hidden">
+			<Box marginTop={2} marginLeft={2} width={4} height={4} flexShrink={0}>
+				<Text>
+					AAAA{'\n'}BBBB{'\n'}CCCC{'\n'}DDDD
+				</Text>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, '\n\n  AA\n  BB');
+});
+
+test('nested overflow', t => {
+	const output = renderToString(
+		<Box paddingBottom={1}>
+			<Box width={4} height={4} overflow="hidden" flexDirection="column">
+				<Box width={2} height={2} overflow="hidden">
+					<Box width={4} height={4} flexShrink={0}>
+						<Text>
+							AAAA{'\n'}BBBB{'\n'}CCCC{'\n'}DDDD
+						</Text>
+					</Box>
+				</Box>
+
+				<Box width={4} height={3}>
+					<Text>
+						XXXX{'\n'}YYYY{'\n'}ZZZZ
+					</Text>
+				</Box>
+			</Box>
+		</Box>
+	);
+
+	t.is(output, 'AA\nBB\nXXXX\nYYYY\n');
+});
