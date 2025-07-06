c41b54a679395efcc9164e3435389c95b746b59b
Noritaka Kobayashi <arukimedesu1166777@gmail.com>
2025-07-06 21:16:20 +0900

    Fix typos (#728)

--- src/hooks/use-input.ts
+++ src/hooks/use-input.ts
@@ -182,7 +182,7 @@ const useInput = (inputHandler: Handler, options: Options = {}) => {
 
 			// If app is not supposed to exit on Ctrl+C, then let input listener handle it
 			if (!(input === 'c' && key.ctrl) || !internal_exitOnCtrlC) {
-				// @ts-expect-error TypeScript types for `batchedUpdates` require an argument, but React's codebase doesn't provide it and it works without it as exepected.
+				// @ts-expect-error TypeScript types for `batchedUpdates` require an argument, but React's codebase doesn't provide it and it works without it as expected.
 				reconciler.batchedUpdates(() => {
 					inputHandler(input, key);
 				});
