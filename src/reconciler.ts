f42a17a10474bad22b3feb5487458223d6ba7e36
christine betts <chrstn@uw.edu>
2025-07-30 11:37:43 -0400

    Remove async issue

--- src/reconciler.ts
+++ src/reconciler.ts
@@ -33,7 +33,7 @@ if (process.env['DEV'] === 'true') {
 		if (error.code === 'ERR_MODULE_NOT_FOUND') {
 			console.warn(
 				`
-The environment variable DEV is set to true, so Ink tried to import \`react-devtools-core\`,
+The environment variable DEV is set to true, so Ink tried to import react-devtools-core,
 but this failed as it was not installed. Debugging with React Devtools requires it.
 
 To install use this command:
