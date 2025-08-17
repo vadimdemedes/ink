557ed2234ce82306c75a4f86441987f75911a342
Sindre Sorhus <sindresorhus@gmail.com>
2025-08-18 01:38:42 +0700

    Fix inconsistent width handling for certain characters
    Fixes #748

---
+++ test/text-width.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import test from 'ava';
+import {Box, Text} from '../src/index.js';
+import {renderToString} from './helpers/render-to-string.js';
+
+test('wide characters do not add extra space inside fixed-width Box', t => {
+	const output = renderToString(
+		<Box flexDirection="column">
+			<Box>
+				<Box width={2}>
+					<Text>🍔</Text>
+				</Box>
+				<Text>|</Text>
+			</Box>
+			<Box>
+				<Box width={2}>
+					<Text>⏳</Text>
+				</Box>
+				<Text>|</Text>
+			</Box>
+		</Box>,
+	);
+
+	const lines = output.split('\n');
+	// Both lines should have the pipe directly after the 2-column box
+	t.is(lines[0], '🍔|');
+	t.is(lines[1], '⏳|');
+});
