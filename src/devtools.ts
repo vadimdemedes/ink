dce2500d66605b98310aa6bc151acf07d01e0993
Markus Olsson <j.markus.olsson@gmail.com>
2025-09-18 09:48:26 +0200

    Initialize devtools
    Necessary as of https://github.com/facebook/react/commit/a86afe8e560f452a9df5ceb4893d9423e5840800

--- src/devtools.ts
+++ src/devtools.ts
@@ -8,4 +8,5 @@ import './devtools-window-polyfill.js';
 import devtools from 'react-devtools-core';
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+(devtools as any).initialize();
 (devtools as any).connectToDevTools();
