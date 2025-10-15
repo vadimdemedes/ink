9a06d8901893d626bbcd972d489cfe96a0d5f5a4
Jason Wu <jason.wu2153@gmail.com>
2025-10-15 01:57:51 -0700

    fix: shrink bug

--- src/log-update.ts
+++ src/log-update.ts
@@ -46,7 +46,7 @@ const create = (stream: Writable, {showCursor = false} = {}): LogUpdate => {
 		if (lineCount < previousLineCount) {
 			buffer.push(
 				ansiEscapes.eraseLines(previousLineCount - lineCount + 1),
-				ansiEscapes.cursorUp(lineCount),
+				ansiEscapes.cursorUp(lineCount - 1),
 			);
 		} else {
 			buffer.push(ansiEscapes.cursorUp(previousLineCount - 1));
