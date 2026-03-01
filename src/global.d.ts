b50714a3a65f0dddf477178ebb230b256a4afe08
HichemF <fantar.hichem@gmail.com>
2026-03-01 10:52:25 +0100

    Replace LegacyRef with Ref for improved type safety in global.d.ts

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,4 @@
-import {type ReactNode, type Key, type LegacyRef} from 'react';
+import {type ReactNode, type Key, type Ref} from 'react';
 import {type Except} from 'type-fest';
 import {type DOMElement} from './dom.js';
 import {type Styles} from './styles.js';
@@ -18,8 +18,7 @@ declare namespace Ink {
 		internal_static?: boolean;
 		children?: ReactNode;
 		key?: Key;
-		// eslint-disable-next-line @typescript-eslint/no-deprecated
-		ref?: LegacyRef<DOMElement>;
+		ref?: Ref<DOMElement>;
 		style?: Except<Styles, 'textWrap'>;
 		internal_accessibility?: DOMElement['internal_accessibility'];
 	};
