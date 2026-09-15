ece94b8bf3c55bc6447ffffc79a503577d499a3c
Daniel Leong <me@dhleong.net>
2026-09-14 20:06:47 -0400

    Add a brief docstring

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -14,6 +14,11 @@ export type Props = {
 	readonly shape?: keyof typeof shapeToCode;
 };
 
+/**
+ * Alternative to useCursor hook that may be placed inline amongst
+ * `<Text>` elements to determine cursor position implicitly based
+ * on text wrapping, etc.
+ */
 export default function Cursor({shape = 'block'}: Props) {
 	const code = shapeToCode[shape];
 	if (code === undefined) {
